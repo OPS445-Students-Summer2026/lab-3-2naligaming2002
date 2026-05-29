9b4e833e2365a6d998a3cd36fce6fc1be487939e
Manish <mneupane5@myseneca.ca>
2026-05-29 12:32:36 -0400

    Fixed lab3f

--- lab3f.py
+++ lab3f.py
@@ -2,14 +2,21 @@
 
 my_list = [1, 2, 3, 4]
 
-def add_item_to_list(lst):
-    next_num = max(lst) + 1
-    lst.append(next_num)
-    lst.append(next_num + 1)
-    return lst
+def add_item_to_list(list_name):
 
-def remove_items_from_list(lst, items):
+    if max(list_name) < 8:
+        list_name.append(max(list_name) + 1)
+        list_name.append(max(list_name) + 1)
+
+def remove_items_from_list(list_name, items):
+
     for item in items:
-        if item in lst:
-            lst.remove(item)
-    return lst
+        if item in list_name:
+            list_name.remove(item)
+
+if __name__ == '__main__':
+    add_item_to_list(my_list)
+    print(my_list)
+
+    remove_items_from_list(my_list, [1, 2])
+    print(my_list)
